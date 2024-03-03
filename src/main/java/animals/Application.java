e846a493e776399a6c46081c3a12b223392fd364
Story81 <story81@bk.ru>
2024-03-03 20:52:54 +0400

    1

--- src/main/java/animals/Application.java
+++ src/main/java/animals/Application.java
@@ -1,2 +1,21 @@
-package animals;public class Application {
+package animals;
+
+public class Application {
+    public static void main(String[] args) {
+        Kotic kotic = new Kotic("Барсик", "мяу", 3, 3);
+
+        Kotic kotic2 = new Kotic();
+        kotic2.setName("Васька");
+        kotic2.setVoice("мя");
+        kotic2.setSatiety(5);
+        kotic2.setWeight(2);
+
+        kotic.liveAnotherDay();
+
+        System.out.println("Вес котика "+ kotic2.getName() + " равен " + kotic2.getWeight());
+
+        System.out.println("Число созданных объектов: " + Kotic.getCount());
+
+        System.out.println("Котики мяукуют одинаково? " + Kotic.compareVoice(kotic, kotic2));
+    }
 }
